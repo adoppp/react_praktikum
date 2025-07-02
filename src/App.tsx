4ee668cdcd533e15c5a5cdbb871054353b90a451
Vlad <vladdev461@gmail.com>
2025-07-02 18:37:34 +0300

    test(app): testing routs

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <Routes>
       {
         routes.map(({ path, element }) => (
-        <Route key={path} path={path} element={element} />
+          <Route key={path} path={path} element={element} />
         ))
       }
     </Routes>
