af958ef509c42f7a12e426af2cdfd189d74845e8
Vlad <vladdev461@gmail.com>
2025-07-05 14:55:15 +0300

    feature(assets): svg added

---
+++ src/assets/svg/index.tsx
@@ -0,0 +1,89 @@
+export const IconPlus = (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1.25 10H18.75" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M10 18.75L10 1.25" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+);
+
+export const IconProfile = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+    <path d="M30.3 31.95C30.125 31.925 29.9 31.925 29.7 31.95C25.3 31.8 21.8 28.2 21.8 23.775C21.8 19.25 25.45 15.575 30 15.575C34.525 15.575 38.2 19.25 38.2 23.775C38.175 28.2 34.7 31.8 30.3 31.95Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M46.85 48.45C42.4 52.525 36.5 55 30 55C23.5 55 17.6 52.525 13.15 48.45C13.4 46.1 14.9 43.8 17.575 42C24.425 37.45 35.625 37.45 42.425 42C45.1 43.8 46.6 46.1 46.85 48.45Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M30 55C43.8071 55 55 43.8071 55 30C55 16.1929 43.8071 5 30 5C16.1929 5 5 16.1929 5 30C5 43.8071 16.1929 55 30 55Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+);
+
+export const IconSignOut = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+    <path d="M6.40006 15.4001H2.20009C1.77574 15.4001 1.36878 15.2268 1.06872 14.9185C0.768667 14.6101 0.600098 14.1918 0.600098 13.7557V2.24454C0.600098 1.80841 0.768667 1.39014 1.06872 1.08174C1.36878 0.773351 1.77574 0.600098 2.20009 0.600098H6.40006" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M11.4004 11.9999L15.4004 7.99997L11.4004 4" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M15.3997 8H5.7998" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+);
+
+export const IconEllipsis = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="4" viewBox="0 0 20 4" fill="none">
+    <rect y="4" width="4" height="4" rx="2" transform="rotate(-90 0 4)" />
+    <rect x="8" y="4" width="4" height="4" rx="2" transform="rotate(-90 8 4)" />
+    <rect x="16" y="4" width="4" height="4" rx="2" transform="rotate(-90 16 4)" />
+  </svg>
+);
+
+export const IconCross = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+    <path d="M1.00012 1L15.2419 15.0418" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M15.2417 1L0.999904 15.0418" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+);
+
+export const IconTrashBin = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+    <rect x="3" y="3.3999" width="10" height="12" rx="1" stroke-width="1.2" />
+    <rect x="1.5" y="1.3999" width="13" height="2" rx="0.5" stroke-width="1.2" stroke-linejoin="round" />
+    <path d="M6 0.600098L10 0.600098" stroke-width="1.2" stroke-linecap="round" />
+    <path d="M8 6V13" stroke-width="1.2" stroke-linecap="round" />
+    <path d="M10.5 6V13" stroke-width="1.2" stroke-linecap="round" />
+    <path d="M5.5 6V13" stroke-width="1.2" stroke-linecap="round" />
+  </svg>
+);
+
+export const IconGear = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+    <path d="M9.4681 14.7592C9.4681 14.7589 9.46801 14.7586 9.46785 14.7583L9.28248 14.4256C8.72272 13.4209 7.27748 13.4209 6.71771 14.4256L6.5321 14.7587C6.39949 14.9965 6.18125 15.17 5.92532 15.2411C5.66939 15.3123 5.39669 15.2752 5.1671 15.1381L3.4341 14.1016C3.20442 13.9641 3.03684 13.7377 2.9682 13.4722C2.89956 13.2067 2.9355 12.9238 3.0681 12.6857L3.29358 12.281C3.85065 11.2812 3.12778 10.051 1.98324 10.051H1.6001C1.33488 10.051 1.08053 9.94177 0.892991 9.74739C0.705455 9.553 0.600098 9.28937 0.600098 9.01447V6.94148C0.600098 6.66659 0.705455 6.40295 0.892991 6.20857C1.08053 6.01419 1.33488 5.90499 1.6001 5.90499H1.98324C3.12778 5.90499 3.85065 4.67474 3.29358 3.67492L3.0681 3.27022C3.00228 3.1523 2.95954 3.02209 2.94232 2.88704C2.92509 2.75198 2.93373 2.61473 2.96772 2.48314C3.00171 2.35154 3.0604 2.22818 3.14042 2.12012C3.22045 2.01205 3.32024 1.92139 3.4341 1.85333L5.1661 0.81684C5.39578 0.679399 5.66872 0.642153 5.9249 0.713295C6.18107 0.784438 6.39949 0.958142 6.5321 1.1962L6.69008 1.47986C7.26189 2.50655 8.73869 2.50676 9.31079 1.48024L9.4691 1.1962C9.60171 0.958423 9.81994 0.784921 10.0759 0.713793C10.3318 0.642665 10.6045 0.679727 10.8341 0.81684L12.5671 1.85333C12.7965 1.99078 12.9639 2.21698 13.0325 2.48225C13.1011 2.74752 13.0654 3.03018 12.9331 3.26815L12.7065 3.67497C12.1494 4.6748 12.8723 5.90499 14.0168 5.90499H14.4001C14.6653 5.90499 14.9197 6.01419 15.1072 6.20857C15.2947 6.40295 15.4001 6.66659 15.4001 6.94148V9.01447C15.4001 9.28937 15.2947 9.553 15.1072 9.74739C14.9197 9.94177 14.6653 10.051 14.4001 10.051H14.0165C12.872 10.051 12.1492 11.281 12.706 12.2808L12.9321 12.6868C13.0647 12.9248 13.1006 13.2077 13.032 13.4733C12.9634 13.7388 12.7958 13.9652 12.5661 14.1026L10.8341 15.1391C10.6044 15.2766 10.3315 15.3138 10.0753 15.2427C9.81929 15.1716 9.60099 14.998 9.46835 14.7602C9.46818 14.7599 9.4681 14.7596 9.4681 14.7592V14.7592Z" stroke-width="1.2" stroke-linejoin="round" />
+    <circle cx="8" cy="8" r="2.4" stroke-width="1.2" />
+  </svg>
+);
+
+export const IconWatch = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+    <path d="M15.3499 7.9999C15.3499 3.94061 12.0592 0.649902 7.9999 0.649902C3.94061 0.649902 0.649902 3.94061 0.649902 7.9999C0.649902 12.0592 3.94061 15.3499 7.9999 15.3499C12.0592 15.3499 15.3499 12.0592 15.3499 7.9999Z" stroke-width="1.2" stroke-miterlimit="10" />
+    <path d="M7.469 3.7561V8.9701L10.9998 11.0001" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+);
+
+export const IconCheckmark = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="8" height="6" viewBox="0 0 8 6" fill="none">
+    <path d="M6.7032 1.23223L3.16767 4.76777L1.3999 3" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+);
+
+export const IconArrow = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="9" viewBox="0 0 14 9" fill="none">
+    <path d="M0.75 1.5L7 7.75L13.25 1.5" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+);
+
+export const IconPensil = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M13.2601 3.59997L5.0501 12.29C4.7401 12.62 4.4401 13.27 4.3801 13.72L4.0101 16.96C3.8801 18.13 4.7201 18.93 5.8801 18.73L9.1001 18.18C9.5501 18.1 10.1801 17.77 10.4901 17.43L18.7001 8.73997C20.1201 7.23997 20.7601 5.52997 18.5501 3.43997C16.3501 1.36997 14.6801 2.09997 13.2601 3.59997Z" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M11.8899 5.05005C12.3199 7.81005 14.5599 9.92005 17.3399 10.2" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M3 22H21" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+);
+
+export const IconEye = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M15.5799 11.9999C15.5799 13.9799 13.9799 15.5799 11.9999 15.5799C10.0199 15.5799 8.41992 13.9799 8.41992 11.9999C8.41992 10.0199 10.0199 8.41992 11.9999 8.41992C13.9799 8.41992 15.5799 10.0199 15.5799 11.9999Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M12.0001 20.27C15.5301 20.27 18.8201 18.19 21.1101 14.59C22.0101 13.18 22.0101 10.81 21.1101 9.39997C18.8201 5.79997 15.5301 3.71997 12.0001 3.71997C8.47009 3.71997 5.18009 5.79997 2.89009 9.39997C1.99009 10.81 1.99009 13.18 2.89009 14.59C5.18009 18.19 8.47009 20.27 12.0001 20.27Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+);
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,20 @@
 import type { FC, ReactElement } from 'react';
 import classNames from 'classnames/bind';
+import { IconPlus } from '@/assets/svg';
 import styles from './Dashboard.module.scss';
 
 const cn = classNames.bind(styles);
 
-interface DashboardProps {};
+interface DashboardProps { };
 
 const Dashboard: FC<DashboardProps> = (): ReactElement => {
   return (
     <div className={cn('classes')}>
       Dashboard
+      {/* <img src='/src/assets/svg/icon-plus.svg'></img> */}
+      <div className={cn('parent')}>
+        {IconPlus}
+      </div>
     </div>
   );
 };
